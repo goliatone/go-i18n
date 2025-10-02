d2affd63f6dc6a90319acc33d367bfc33484f3c6
goliatone <140582+goliatone@users.noreply.github.com>
2025-10-02 15:17:41 -0700

    chore: update docs

--- errors.go
+++ errors.go
@@ -5,5 +5,5 @@ import "errors"
 // ErrMissingTranslation indicates that no translation was found for locale/key.
 var ErrMissingTranslation = errors.New("i18n: missing translation")
 
-// ErrNotImplemented marks APIs that are intentionally stubbed during bootstrapping
+// ErrNotImplemented marks APIs that are intentionally stubbed during bootstrapping.
 var ErrNotImplemented = errors.New("i18n: not implemented")
